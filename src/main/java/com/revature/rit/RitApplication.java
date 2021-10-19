335df01c26af75f0a2e0339e94eb550813e2fbe3
Ryan Mohler <ryan.mohler17@gmail.com>
2021-10-19 15:49:15 -0500

    Fix typo

--- src/main/java/com/revature/rit/RitApplication.java
+++ src/main/java/com/revature/rit/RitApplication.java
@@ -1,4 +1,4 @@
-package com.reavature.rit;
+package com.revature.rit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,4 +9,5 @@ public class RitApplication {
     public static void main(String[] args) {
         SpringApplication.run(RitApplication.class, args);
     }
+
 }
