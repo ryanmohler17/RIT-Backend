353deb7a3bf8354d1906c42afd1d2731ff2ce7d8
Ryan Mohler <ryan.mohler17@gmail.com>
2021-10-27 06:55:06 -0500

    More testing

--- src/main/java/com/revature/rit/models/issues/request/IssueInput.java
+++ src/main/java/com/revature/rit/models/issues/request/IssueInput.java
@@ -1,10 +1,15 @@
 package com.revature.rit.models.issues.request;
 
 import com.revature.rit.models.users.User;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class IssueInput {
     private String title;
     private String description;
--- src/test/java/com/revature/rit/controllers/IssueControllerTest.java
+++ src/test/java/com/revature/rit/controllers/IssueControllerTest.java
@@ -1,25 +1,33 @@
 package com.revature.rit.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.rit.models.issues.Issue;
 import com.revature.rit.models.issues.request.CommentInput;
 import com.revature.rit.models.issues.request.IssueInput;
 import com.revature.rit.models.users.User;
+import com.revature.rit.models.users.UserLevel;
 import com.revature.rit.reposistory.*;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -49,17 +57,19 @@ class IssueControllerTest {
     PlatformTransactionManager transactionManager;
 
     @Test
-    void createIssue() {
-        IssueController c = new IssueController();
-        ResponseEntity<User> result = null;
-
-        try {
-            result = c.createIssue(new IssueInput());
-        } catch (Exception e) {
-
-        }
-
-        Assert.assertEquals(result, result);
+    void createIssue() throws Exception {
+        Mockito.when(userRepository.findById(Mockito.anyInt())).then(invocation -> {
+            return Optional.of(new User("test", "test", "test", UserLevel.User));
+        });
+        Mockito.when(issueRepository.save(Mockito.any())).then(invocation -> {
+            return invocation.getArgument(0);
+        });
+        User user = new User("test", "test", "test", UserLevel.User);
+        user.setId(1);
+        IssueInput issueInput = new IssueInput("test", "test", "Technical", user, "Medium", "Open");
+        String uri = "/issues/createIssue";
+        mockMvc.perform(MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON).content(new ObjectMapper().writeValueAsString(issueInput)))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
     }
 
     @Test
